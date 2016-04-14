8b86c3675dce66a60643ddcfcbe8e83691893b36
Michael Rahija <michaelrahija@Michaels-MBP-2.lan>
2016-04-14 21:26:56 +0200

    removed leading and trailing spaces on donor names for donor aggregations

--- R/computeDonorContrib.R
+++ R/computeDonorContrib.R
@@ -1,38 +1,54 @@
-#function to take data frame containing donor, country, budget, and agId
-#return dataframe with 1 row for each combination of 1 donor w/ column for 
-#% contribution to the agId
+##' Seperates donors into unique rows with a column for percentage of
+##' budget
+##' 
+##' The function takes a clean df (after running cleanFigures.R)
+##' and returns a df with country, budget, agId, key (donor#), donor, percent
+##' 
+##' @param data is a cleaned data frame
+##' 
+##' @return a data frame
+##'
+##' @export
+##'
 
-##https://blog.rstudio.org/2014/07/22/introducing-tidyr/
+computeDonorContrib <- function(df = data){
+  
 
-df <- select(data, donor, country, budget,agId)
+  #take only relevent columns
+  df <- select(data, donor, country, budget,agId)
+  
+  #create column names for spreading donor
+  donor.cols <- paste0("Donor",1:11)
+  
+  #seperate donor column
+  test <- separate(df, donor,
+                   into = donor.cols,
+                    sep = ",",
+                    extra = 'drop')
+  
+  #gather into one column called donor. keep relevant columns
+  x <- test %>%
+          gather(key, donor,-country,-budget, - agId, na.rm = TRUE)
+  
+  
+  x.arr <- arrange(x,agId)
+  
+  #create percentage variable for budget allocation
+  x.arr <- separate(x.arr, donor,
+                    into = c("donor","percent"),
+                    sep = "\\(",
+                    extra = "drop")
+  
+  x.arr$percent <- gsub("\\%","",x.arr$percent)
+  x.arr$percent <- gsub("\\)","",x.arr$percent)
+  x.arr$percent[is.na(x.arr$percent)] <- 100
+  x.arr$percent <- as.numeric(x.arr$percent)/100
+  
+  #trim white space from donor names 
+  #taken from http://stackoverflow.com/questions/2261079/how-to-trim-leading-and-trailing-whitespace-in-r
+  trim <- function (x) gsub("^\\s+|\\s+$", "", x)
+  x.arr$donor <- trim(x.arr$donor)
 
+x.arr    
 
-donor.cols <- paste0("Donor",1:11)
-
-test <- separate(df, donor,
-                 into = donor.cols,
-                  sep = ",",
-                  extra = 'drop')
-
-head(test)
-tail(test)
-x <- test %>%
-        gather(key, donor,-country,-budget, - agId)
-
-x <- filter(x, !is.na(donor))
-
-x.arr <- arrange(x,agId)
-
-
-
-head(x.arr)
-test2 <- gather(test, key = agId, country,value = budget)
-head(test2)
-
-test2 <- gather(test,country,budget,agId, na.rm = TRUE)
-
-
-names(test2) <- c("countries","budget","Donor")
-head(test2)
-
-ar.test <- arrange(test2,country)
+}  
